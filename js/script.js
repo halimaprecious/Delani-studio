af197525f257c6b79329a9d7605cbd06892827cd
halima precious <halimaprecious3@gmail.com>
2022-03-14 14:15:01 +0300

    feat;added hover effect to portfolio section

--- js/script.js
+++ js/script.js
@@ -12,6 +12,44 @@ $(document).ready(function () {
         $("#img-prod").toggle();
         $("#manp").toggle();
     });
+    // portfolio
+    $(".img_w4").hover(function () {
+        $("#lay1").toggle();
+        
+    });
+    $(".img_w3").hover(function () {
+        $("#lay2").toggle();
+        
+    });
+        
+    $(".img_w2").hover(function () {
+        $("#lay3").toggle();
+        
+    });
+        
+    $(".img_w1").hover(function () {
+        $("#lay4").toggle();
+        
+    });
+    $(".img_w5").hover(function () {
+        $("#over1").toggle();
+        
+    });
+    $(".img_w6").hover(function () {
+        $("#over2").toggle();
+        
+    });
+        
+    $(".img_w7").hover(function () {
+        $("#over3").toggle();
+        
+    });
+        
+    $(".img_w8").hover(function () {
+        $("#over4").toggle();
+        
+    });
+        
 });
 // form
 document.querySelector("#formData").onsubmit=()=>{
